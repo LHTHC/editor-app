1c25627e8cdd17efff8886aa660d1ab52b218469
LHTHC <starfall1011@gmail.com>
2022-11-09 12:07:47 +0300

    added readonly and data-field

--- main.js
+++ main.js
@@ -14,58 +14,72 @@ import DragDrop from "editorjs-drag-drop";
 
 import "./style.css";
 
-const editor = new EditorJS({
-  onReady: () => {
-    new DragDrop(editor);
-  },
-  holder: "editorjs",
-  tools: {
-    header: {
-      class: Header,
-      config: {
-        placeholder: "Введите заголовок",
-        levels: [1, 2, 3, 4],
-        defaultLevel: 2,
+const configureEditor = (holder, readOnly = false) => {
+  return {
+    onReady: () => {
+      new DragDrop(editor);
+    },
+    holder: holder,
+    readOnly: readOnly,
+    tools: {
+      header: {
+        class: Header,
+        config: {
+          placeholder: "Введите заголовок",
+          levels: [1, 2, 3, 4],
+          defaultLevel: 2,
+        },
+      },
+      image: SimpleImage,
+      checklist: {
+        class: Checklist,
+        inlineToolbar: true,
+      },
+      raw: RawTool,
+      list: {
+        class: List,
+        inlineToolbar: true,
+        config: {
+          defaultStyle: "unordered",
+        },
+      },
+      embed: Embed,
+      quote: Quote,
+      code: {
+        class: editorjsCodeflask,
+        showlinenumbers: false,
+      },
+      Marker: {
+        class: Marker,
+        shortcut: "CMD+SHIFT+M",
+      },
+      inlineCode: {
+        class: InlineCode,
       },
     },
-    image: SimpleImage,
-    checklist: {
-      class: Checklist,
-      inlineToolbar: true,
-    },
-    raw: RawTool,
-    list: {
-      class: List,
-      inlineToolbar: true,
-      config: {
-        defaultStyle: "unordered",
-      },
-    },
-    embed: Embed,
-    quote: Quote,
-    code: {
-      class: editorjsCodeflask,
-      showlinenumbers: false,
-    },
-    Marker: {
-      class: Marker,
-      shortcut: "CMD+SHIFT+M",
-    },
-    inlineCode: {
-      class: InlineCode,
-    },
-  },
-  placeholder: "Let`s write an awesome story!",
-});
+    placeholder: "Let`s write an awesome story!",
+  };
+};
+
+const editor = new EditorJS(configureEditor("editorjs", false));
 
 const saveButton = document.querySelector(".btn-save");
+const readOnlyBlock = document.querySelector("#readonly");
+const dataContainer = document.querySelector(".data-container");
 
 saveButton.addEventListener("click", () => {
   editor
     .save()
     .then((data) => {
-      console.log("generated data: ", data);
-      navigator.clipboard.writeText(JSON.stringify(data));
+      readOnlyBlock.innerHTML = "";
+      const readOnlyEditor = new EditorJS(configureEditor("readonly", true));
+      readOnlyEditor.isReady.then(() => {
+        readOnlyEditor.render(data);
+      });
+      const plainData = JSON.stringify(data);
+      localStorage.setItem("pageData", plainData);
+      dataContainer.innerText = plainData;
+      navigator.clipboard.writeText(plainData);
     })
     .catch((error) => {
       console.log("failed", error);
